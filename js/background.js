f94a34a305ea4b3805412cc10164ff3806e2a877
liuwenhao <liuwenhao1@wps.cn>
2022-10-16 19:47:29 +0800

    feat: 支持更多标签页管理形式

--- js/background.js
+++ js/background.js
@@ -1,58 +1,151 @@
 chrome.contextMenus.create({
     id: "1",
-    title: `打开主页`,
+    title: `发送当前标签页`,
     contexts: ["browser_action"],
     onclick: function () {
-        chrome.tabs.create({ index: 0, url: chrome.extension.getURL('pages/index.html') });
-    }
+        chrome.tabs.query({ url: ["https://*/*", "http://*/*"], highlighted: true, currentWindow: true }, function (req) {
+            if (req.length > 0) {
+                saveTabList(req)
+            }
+        });
+    },
 });
 
 chrome.contextMenus.create({
     id: "2",
-    title: `标签管理`,
+    title: `更多操作`,
     contexts: ["browser_action"]
 });
 
 chrome.contextMenus.create({
-    id: "3",
-    title: `发送当前标签`,
+    id: "2-1",
+    parentId: "2",
+    title: `发送全部标签页`,
     contexts: ["browser_action"],
     onclick: function () {
-        chrome.storage.local.get(function (storage) {
-            let opts = storage.options
-            chrome.tabs.query({ url: ["https://*/*", "http://*/*"], highlighted: true, currentWindow: true }, function (req) {
-                // 拿到标签后，存储到浏览器，然后在react中获取并展示
-                if (req.length > 0) {
-                    console.log(req, storage)
-                    saveTabList(req)
-                    // chrome.tabs.reload(req[0].id, {}, function (tab) {
-                    //     console.log('after reload', tab)
-                    // });
+        chrome.tabs.query({ url: ["https://*/*", "http://*/*"], currentWindow: true }, function (req) {
+            if (req.length > 0) {
+                saveTabList(req)
+            }
+        });
+    }
+});
+
+chrome.contextMenus.create({
+    id: "2-2",
+    parentId: "2",
+    title: `发送左侧标签页`,
+    contexts: ["browser_action"],
+    onclick: function () {
+        chrome.tabs.query({ url: ["https://*/*", "http://*/*"], currentWindow: true }, function (req) {
+            const leftTabs = [];
+            for (let item of req) {
+                if (item.active) {
+                    break;
                 }
-            });
-        })
+                leftTabs.push(item)
+            }
+            if (leftTabs.length > 0) {
+                saveTabList(leftTabs)
+            }
+        });
+    }
+});
+
+chrome.contextMenus.create({
+    id: "2-3",
+    parentId: "2",
+    title: `发送右侧标签页`,
+    contexts: ["browser_action"],
+    onclick: function () {
+        chrome.tabs.query({ url: ["https://*/*", "http://*/*"], currentWindow: true }, function (req) {
+            let rightTabs = [];
+            let activeIndex
+            for (let item of req) {
+                if (item.active) {
+                    activeIndex = item.index;
+                }
+            }
+            if (activeIndex) {
+                rightTabs = req.filter(item => item.index > activeIndex)
+            }
+            if (rightTabs.length > 0) {
+                saveTabList(rightTabs)
+            }
+        });
+    }
+});
+
+chrome.contextMenus.create({
+    id: "2-4",
+    parentId: "2",
+    title: `发送其他标签页`,
+    contexts: ["browser_action"],
+    onclick: function () {
+        chrome.tabs.query({ url: ["https://*/*", "http://*/*"], active: false, currentWindow: true }, function (req) {
+            if (req.length > 0) {
+                saveTabList(req)
+            } else {
+                openLocalPage('pages/index.html')
+            }
+        });
+    }
+});
+
+chrome.contextMenus.create({
+    type: 'separator',
+    contexts: ["browser_action"],
+    parentId: "2"
+});
+
+chrome.contextMenus.create({
+    id: "2-5",
+    title: `打开主页`,
+    contexts: ["browser_action"],
+    onclick: function () {
+        // chrome.tabs.create({ index: 0, url: chrome.extension.getURL('pages/index.html') });
+        openLocalPage(`pages/index.html`)
     },
     parentId: "2"
 });
 
 
-const saveTabList = (tabs) => {
-    const date = dateFormat("YYYY-mm-dd HH:MM:SS", new Date())
-    tabs = tabs.map(({ title, url }) => ({ title, url }));
-    const tabItem = {
-        id: generateUUID(),
-        title: tabs[0].title,
-        url: tabs[0].url,
-        date: date
+
+chrome.contextMenus.create({
+    type: 'separator',
+    contexts: ["browser_action"],
+    parentId: "2"
+});
+
+chrome.contextMenus.create({
+    id: "2-6",
+    parentId: "2",
+    title: `帮助中心`,
+    contexts: ["browser_action"],
+    onclick: function () {
+        openLocalPage('help.html')
     }
+});
+
+
+// 保存标签页
+const saveTabList = (tabs) => {
+    if (tabs.length === 0) return;
+    const date = dateFormat("YYYY-mm-dd HH:MM:SS", new Date())
+    tabList = tabs.map(({ title, url }) => ({
+        id: generateUUID(),
+        title,
+        url,
+        date: date
+    }));
+
     chrome.storage.local.get("tabs", (storage) => {
-        // 整合数据，并新增
-        const newtabs = Object.keys(storage).length!==0 ? [...storage.tabs, tabItem] : [tabItem];
+        const newtabs = Object.keys(storage).length !== 0 ? [...storage.tabs, ...tabList] : tabList;
         chrome.storage.local.set({ tabs: newtabs });
-        chrome.tabs.create({ index: 0, url: chrome.extension.getURL('pages/index.html') });
+        closeTabs(tabs)
+        openLocalPage('pages/index.html')
     })
 }
-
 
 // 日期格式化
 function dateFormat(fmt, date) {
@@ -75,8 +168,6 @@ function dateFormat(fmt, date) {
     return fmt;
 }
 
-
-
 // 生成唯一标识
 // refer: https://gist.github.com/solenoid/1372386
 const generateUUID = function () {
@@ -91,5 +182,37 @@ function refresh() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabsArr) {
         chrome.tabs.reload(tabsArr[0].id, {}, function (tab) {
         });
+    });
+}
+
+
+// 打开本地页面
+function openLocalPage(url) {
+    chrome.tabs.query({ url: `chrome-extension://*/${url}*`, currentWindow: true }, function (tab) {
+        if (tab.length >= 1) {
+            chrome.tabs.move(tab[0].id, { index: 0 }, function callback() {
+                chrome.tabs.highlight({ tabs: 0 }, function callback() {
+                });
+            });
+            chrome.tabs.reload(tab[0].id, {}, function (tab) {
+            });
+        } else {
+            chrome.tabs.create({ index: 0, url: chrome.extension.getURL(url) });
+        }
+    });
+}
+
+// 关闭标签页组
+function closeTabs(tabsList) {
+    if (tabsList.length === 0) return;
+    var readyCloseTabs = [];
+    for (let i = 0; i < tabsList.length; i += 1) {
+        readyCloseTabs.push(tabsList[i].id);
+    }
+
+    chrome.tabs.remove(readyCloseTabs, function () {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+        }
     });
 }
